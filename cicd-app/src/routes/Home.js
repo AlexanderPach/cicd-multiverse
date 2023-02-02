0daf0ca234a09fe1bb91ee8fc95c9f3f5240a482
AlexanderPach <33683097+AlexanderPach@users.noreply.github.com>
2023-02-02 03:28:46 -0700

    Update Home.js

--- cicd-app/src/routes/Home.js
+++ cicd-app/src/routes/Home.js
@@ -4,13 +4,10 @@ import { Link } from 'react-router-dom';
 function Home() {
   return <>
 
-      <div className="p-lg-5">
-        <h1 className="display-4">My Own Page</h1>
-        <p className="lead font-weight-normal"><em>- Alexander Pacheco</em></p>
-        <Link className="btn" to="/bio">Find out More About Me</Link>
-      </div>
-      <div className="product-device box-shadow d-none d-md-block"></div>
-      <div className="product-device product-device-2 box-shadow d-none d-md-block"></div>
+      <h1 className="display-4">My Own Page</h1>
+      <p className="lead font-weight-normal"><em>- Alexander Pacheco</em></p>
+      <Link className="btn" to="/bio">Find out More About Me</Link>
+     
     
   </>;
 }
